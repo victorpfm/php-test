8ccf04d6e9b36c1605b25997cab19c39a19d40ae
Victor Paiva <victorpfm@gmail.com>
2021-10-14 21:07:52 -0300

    Implement FileCollection

---
+++ src/FileCollection.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Live\Collection;
+
+/**
+ * File collection
+ *
+ * @package Live\Collection
+ */
+class FileCollection implements CollectionInterface
+{
+    /**
+     * Collection data
+     *
+     * @var array
+     */
+    protected array $data;
+
+    /**
+     * Collection name
+     *
+     * @var string
+     */
+    protected $name = 'collection';
+
+    /**
+     * Constructor
+     */
+    public function __construct($name = 'collection')
+    {
+        $this->name = $name;
+
+        if (!$this->collectionFileExists()) {
+            $this->createEmptyFile();
+        }
+
+        $this->parseFile();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get(string $index, $defaultValue = null)
+    {
+        if (!$this->has($index)) {
+            return $defaultValue;
+        }
+
+        return $this->data[$index];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function set(string $index, $value)
+    {
+        $this->data[$index] = $value;
+
+        $this->persist();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has(string $index)
+    {
+        return isset($this->data[$index]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Persists the current data to the collection file
+     *
+     * @return void
+     */
+    protected function persist()
+    {
+        file_put_contents($this->filePath(), json_encode($this->data));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clean()
+    {
+        $this->data = [];
+        $this->createEmptyFile();
+    }
+
+    /**
+     * Returns a path for the collection file
+     *
+     * @return string
+     */
+    protected function filePath()
+    {
+        return dirname(__DIR__, 1) . "/database/{$this->name}.json";
+    }
+
+    /**
+     * Checks if a collection file exists
+     *
+     * @return bool
+     */
+    protected function collectionFileExists()
+    {
+        return file_exists($this->filePath($this->name));
+    }
+
+    /**
+     * Create an empty collection file
+     *
+     * @return void
+     */
+    protected function createEmptyFile()
+    {
+        file_put_contents($this->filePath(), "{}");
+    }
+
+    /**
+     * Parses a collection file
+     *
+     * @return void
+     */
+    protected function parseFile()
+    {
+        $this->data = json_decode(
+            file_get_contents($this->filePath()),
+            true
+        );
+    }
+}
---
+++ tests/src/FileCollectionTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Live\Collection;
+
+use PHPUnit\Framework\TestCase;
+
+class FileCollectionTest extends TestCase
+{
+    /**
+     * @test
+     * @doesNotPerformAssertions
+     */
+    public function objectCanBeConstructed()
+    {
+        $collection = new FileCollection();
+        return $collection;
+    }
+
+    /**
+     * @test
+     * @depends objectCanBeConstructed
+     * @doesNotPerformAssertions
+     */
+    public function dataCanBeAdded()
+    {
+        $collection = new FileCollection();
+        $collection->set('index1', 'value');
+        $collection->set('index2', 5);
+        $collection->set('index3', true);
+        $collection->set('index4', 6.5);
+        $collection->set('index5', ['data']);
+
+        $collection->clean();
+    }
+
+     /**
+     * @test
+     * @depends dataCanBeAdded
+     */
+    public function dataCanBeRetrieved()
+    {
+        $collection = new FileCollection();
+        $collection->set('index1', 'value');
+
+        $this->assertEquals('value', $collection->get('index1'));
+        $collection->clean();
+    }
+
+    /**
+     * @test
+     * @depends objectCanBeConstructed
+     */
+    public function inexistentIndexShouldReturnDefaultValue()
+    {
+        $collection = new FileCollection();
+
+        $this->assertNull($collection->get('index1'));
+        $this->assertEquals('defaultValue', $collection->get('index1', 'defaultValue'));
+    }
+
+    /**
+     * @test
+     * @depends dataCanBeAdded
+     */
+    public function collectionWithItemsShouldReturnValidCount()
+    {
+        $collection = new FileCollection();
+        $collection->set('index1', 'value');
+        $collection->set('index2', 5);
+        $collection->set('index3', true);
+
+        $this->assertEquals(3, $collection->count());
+
+        $collection->clean();
+    }
+
+    /**
+     * @test
+     * @depends collectionWithItemsShouldReturnValidCount
+     */
+    public function collectionCanBeCleaned()
+    {
+        $collection = new FileCollection();
+        $collection->set('index', 'value');
+        $this->assertEquals(1, $collection->count());
+
+        $collection->clean();
+        $this->assertEquals(0, $collection->count());
+    }
+
+    /**
+     * @test
+     * @depends dataCanBeAdded
+     */
+    public function addedItemShouldExistInCollection()
+    {
+        $collection = new FileCollection();
+        $collection->set('index', 'value');
+
+        $this->assertTrue($collection->has('index'));
+
+        $collection->clean();
+    }
+}
